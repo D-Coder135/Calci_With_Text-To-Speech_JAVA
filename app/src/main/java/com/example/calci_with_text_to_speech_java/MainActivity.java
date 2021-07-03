c6184655f61728cf409dd7d71f24544e89250e9d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 20:23:55 +0530

    remove unused import statements. Final Commit!

--- app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
+++ app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.calci_with_text_to_speech_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.util.Locale;
 
