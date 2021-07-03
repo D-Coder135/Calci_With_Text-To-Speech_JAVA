a523568d8a688cc050b789b3908b26e76944e6d7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 18:34:31 +0530

    define the objects for the EditText class

--- app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
+++ app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.calci_with_text_to_speech_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText1, editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
