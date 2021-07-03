359552422e54a675f6d2ce4f168b309c67c989b8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 18:34:59 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
+++ app/src/main/java/com/example/calci_with_text_to_speech_java/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.calci_with_text_to_speech_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText1, editText2;
+    Button add, sub, mul, div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
